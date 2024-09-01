93436a0443d14bbd497e4c66cdc8b1892d5b4ac2
cwangg897 <dhkdrb897@gmail.com>
2024-09-01 19:20:09 +0900

    [LS-4] 외부 api 연동 - feign client 적용

---
+++ external/naver-client/src/main/java/com/library/feign/NaverClientConfiguration.java
@@ -0,0 +1,22 @@
+package com.library.feign;
+
+
+import feign.RequestInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+public class NaverClientConfiguration {
+
+    // 인터페이스라서 람다식으로 추상메서드 구현할 때 사용가능
+    //  Java 8에서 도입된 기능으로, 간단한 메서드를 한 줄로 표현할 수 있게 해줍니다.
+    @Bean
+    public RequestInterceptor requestInterceptor(
+        @Value("${external.naver.headers.client-id}") String clientId,
+        @Value("${external.naver.headers.client-secret}") String clientSecret
+    ) {
+        // apply를 구현한거임
+        return requestTemplate -> requestTemplate.header("X-Naver-Client-Id", clientId)
+            .header("X-Naver-Client-Secret", clientSecret);
+    }
+
+}
