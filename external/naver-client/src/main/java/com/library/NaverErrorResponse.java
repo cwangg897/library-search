90ef7ba7829b040ab8942c23c7479451ed0276b3
cwangg897 <dhkdrb897@gmail.com>
2024-09-01 20:59:49 +0900

    [LS-4] 기본생성자 추가

--- external/naver-client/src/main/java/com/library/NaverErrorResponse.java
+++ external/naver-client/src/main/java/com/library/NaverErrorResponse.java
@@ -1,17 +1,17 @@
 package com.library;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class NaverErrorResponse {
 
     private String errorMessage;
     private String errorCode;
-
-    public String getErrorMessage() {
-        return errorMessage;
-    }
-
-    public String getErrorCode() {
-        return errorCode;
-    }
 
     public NaverErrorResponse(String errorMessage, String errorCode) {
         this.errorMessage = errorMessage;
