d171501e9ced8e06764163ea8a14d2255183b32c
cwangg897 <dhkdrb897@gmail.com>
2024-09-15 14:04:00 +0900

    kakao

--- external/naver-client/src/main/java/com/library/feign/NaverClient.java
+++ external/naver-client/src/main/java/com/library/feign/NaverClient.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+// ErrorDecoder도 여기서 시작
 @FeignClient(name = "naverClient", url = "${external.naver.url}", configuration = NaverClientConfiguration.class)
 public interface NaverClient {
 
